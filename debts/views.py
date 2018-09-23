5281e5722d49d5c9b3d9e091ada0172fd77dead1
Wiratmika <wiratmika@gmail.com>
2018-09-23 19:05:36 +0700

    Install pylint and autopep8, add response type ephemeral

--- debts/views.py
+++ debts/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Debt
 
+
 def create(request, is_add):
     try:
         payload = parse(request.POST['text'])
     except ValueError as err:
         return JsonResponse({
+            'response_type': 'ephemeral',
             'text': 'Gagal menambahkan hutang',
             'attachments': [{
                 'text': str(err)
@@ -19,25 +21,28 @@ def create(request, is_add):
     sha1.update(str(time.time()).encode('utf-8'))
 
     debt = Debt.objects.create(
-        transaction_id = sha1.hexdigest()[:5],
-        source = request.POST['user_name'],
-        source_slack_id = request.POST['user_id'],
-        target = payload['user_name'],
-        target_slack_id = payload['user_id'],
-        amount = payload['amount']
+        transaction_id=sha1.hexdigest()[:5],
+        source=request.POST['user_name'],
+        source_slack_id=request.POST['user_id'],
+        target=payload['user_name'],
+        target_slack_id=payload['user_id'],
+        amount=payload['amount']
     )
 
     return JsonResponse({
+        'response_type': 'ephemeral',
         'text': 'Berhasil menambahkan hutang',
         'attachments': [{
             'text': 'Kamu berhutang sejumlah ' + beautify_amount(debt.amount) + ' ke ' + debt.target
         }]
     })
 
+
 def parse(input):
     content = input.split(' ')
     if (len(content) is not 2):
-        raise ValueError('Jumlah parameter terlalu panjang atau terlalu pendek')
+        raise ValueError(
+            'Jumlah parameter terlalu panjang atau terlalu pendek')
 
     user = content[0]
 
@@ -46,6 +51,7 @@ def parse(input):
         'user_id': user[2:user.index('|')],
         'amount': content[1]
     }
+
 
 def beautify_amount(amount):
     return 'Rp ' + '{:,}'.format(int(amount))
