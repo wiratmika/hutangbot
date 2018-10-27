3c7856146821f443ca3beb66ba678e7c2e933cd3
Wiratmika <wiratmika@gmail.com>
2018-10-27 23:35:45 +0700

    Switch to function based view

--- debts/urls.py
+++ debts/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from debts import views
 
 urlpatterns = [
-    url(r'^add/$', views.DebtList.as_view(), {'is_add': True})
+    path('add/', views.add_debt),
 ]
--- debts/views.py
+++ debts/views.py
@@ -1,16 +1,13 @@
-from django.db import transaction
-
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from debts.models import Debt
 from debts.serializers import DebtSerializer
 
 
-class DebtList(APIView):
-    @transaction.atomic
-    def post(self, request, is_add):
-        serializer = DebtSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+@api_view(['POST'])
+def add_debt(request):
+    serializer = DebtSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
