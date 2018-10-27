9817d2e3c3ae35579c07b00f7bfb892e85261491
Wiratmika <wiratmika@gmail.com>
2018-10-27 22:52:14 +0700

    Move representation to serializer

--- debts/serializers.py
+++ debts/serializers.py
@@ -3,7 +3,7 @@
 from rest_framework import serializers
 
 from debts.models import Debt
-from debts.utils import generate_transaction_id
+from debts.utils import beautify_amount, generate_transaction_id
 
 
 class DebtSerializer(serializers.BaseSerializer):
@@ -29,6 +29,48 @@ class DebtSerializer(serializers.BaseSerializer):
             'target': user[user.index('|') + 1:-1],
             'target_slack_id': user[2:user.index('|')],
             'amount': amount
+        }
+
+    def to_representation(self, obj):
+        instruction = (
+            f"Untuk membatalkan, kirim `/hapus {obj.transaction_id}`\n"
+            f"Untuk membayar, ketik `/bayar @{obj.target} {obj.amount}` atau `/bayar {obj.transaction_id}`\n"
+            "Untuk melihat semua hutang/piutang yang kamu punya, kirim `/listhutang`\n"
+            "Untuk melihat semua daftar transaksi, kirim `/listtransaksi`"
+        )
+
+        return {
+            'response_type': 'ephemeral',
+            'attachments': [{
+                'text': 'Berhasil menambahkan hutang!',
+                'fields': [
+                    {
+                        'title': 'ID Transaksi',
+                        'value': obj.transaction_id,
+                        'short': True
+                    },
+                    {
+                        'title': 'Nilai',
+                        'value': beautify_amount(obj.amount),
+                        'short': True
+                    },
+                    {
+                        'title': 'Ke',
+                        'value': obj.target,
+                        'short': True
+                    },
+                    {
+                        'title': 'Jumlah Hutang',
+                        'value': 'Please implement',
+                        'short': True
+                    },
+                    {
+                        'title': 'Keterangan',
+                        'value': instruction
+                    }
+                ],
+                'color': 'good'
+            }]
         }
 
     def create(self, validated_data):
--- debts/views.py
+++ debts/views.py
@@ -7,7 +7,6 @@ from rest_framework.views import APIView
 
 from debts.models import Debt
 from debts.serializers import DebtSerializer
-from debts.utils import beautify_amount
 
 
 class DebtList(APIView):
@@ -30,48 +29,4 @@ class DebtList(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
-        transaction_id = serializer.validated_data['transaction_id']
-        target = serializer.validated_data['target']
-        amount = serializer.validated_data['amount']
-
-        # TODO: Make it a function
-        instruction = (
-            f"Untuk membatalkan, kirim `/hapus {transaction_id}`\n"
-            f"Untuk membayar, ketik `/bayar @{target} {amount}` atau `/bayar {transaction_id}`\n"
-            "Untuk melihat semua hutang/piutang yang kamu punya, kirim `/listhutang`\n"
-            "Untuk melihat semua daftar transaksi, kirim `/listtransaksi`"
-        )
-
-        return Response({
-            'response_type': 'ephemeral',
-            'attachments': [{
-                'text': 'Berhasil menambahkan hutang!',
-                'fields': [
-                    {
-                        'title': 'ID Transaksi',
-                        'value': transaction_id,
-                        'short': True
-                    },
-                    {
-                        'title': 'Nilai',
-                        'value': beautify_amount(amount),
-                        'short': True
-                    },
-                    {
-                        'title': 'Ke',
-                        'value': target,
-                        'short': True
-                    },
-                    {
-                        'title': 'Jumlah Hutang',
-                        'value': 'Please implement',
-                        'short': True
-                    },
-                    {
-                        'title': 'Keterangan',
-                        'value': instruction
-                    }
-                ],
-                'color': 'good'
-            }]
-        }, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
